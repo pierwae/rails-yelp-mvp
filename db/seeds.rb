a1e45e53283a438162dbcebbd40697fb579f6ccd
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-12 12:11:56 +0200

    seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 fake restaurants...'
+
+5.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name,
+                              address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+                              category: %w[chinese italian japanese french belgian].sample,
+                              phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+  restaurant.save!
+end
+
+puts 'Finished!'
